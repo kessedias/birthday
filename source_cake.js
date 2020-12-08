86b0b4c843ca66b16ac1ccbd3743d7372bc9fad5
Kesse Dias <kesse.dias97@outlook.com>
2020-12-08 19:32:42 -0300

    :sparkles: feat: Adicionando easteregg

--- source_cake.js
+++ source_cake.js
@@ -1,6 +1,28 @@
-window.open = function() {};
-window.print = function() {};
-// Support hover state for mobile.
-if (false) {
-    window.ontouchstart = function() {};
-}
+window.onclick = changeQuarto;
+
+
+function changeQuarto(){
+    var primeiro = document.getElementById('primeiro');
+    primeiro.innerHTML = 'E';
+    var segundo = document.getElementById('segundo');
+    segundo.innerHTML = 'U';
+    var terceiro = document.getElementById('terceiro');
+    terceiro.innerHTML = ' ';
+    var quarto = document.getElementById('quarto');
+   quarto.innerHTML = 'T';
+   var quinto = document.getElementById('quinto');
+    quinto.innerHTML = 'E';
+    var sexto = document.getElementById('sexto');
+    sexto.innerHTML = ' ';
+    var setimo = document.getElementById('setimo');
+    setimo.innerHTML = 'A';
+    var oitavo = document.getElementById('oitavo');
+    oitavo.innerHTML = 'M';
+    var nono = document.getElementById('nono');
+    nono.innerHTML = 'O';
+};
+
+
+
+
+
